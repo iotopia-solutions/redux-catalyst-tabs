f923d32d7e2cd7a06e8cbcb31dc3dbb6543d6343
Helen Thi <ht5658@gmail.com>
2016-03-14 21:10:04 -0400

    set up of dev configurations

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/tabs.scss';
+
+class Tabs extends React.Component {
+  render() {
+    return(
+      <h1>SO COOL!</h1>
+    );
+  }
+}
+
+ReactDOM.render( <Tabs />, document.getElementById("main"));
