aefcf87cc61d92bfa7419027f76e8bc075d1c771
Helen Thi <ht5658@gmail.com>
2016-04-15 16:05:47 -0400

    added babel config to package.json to compile source code to es5 in lib/; .npmignore file added to ignore files in src/

--- src/components/tab.js
+++ src/components/tab.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
-
 export default ({ children, active, tabID, makeActive }) => (
-  <button className="tab"  onClick={() => makeActive(tabID) }>
+  <button className="tab" onClick={() => makeActive(tabID) }>
     { active ? "ALIVE!!" : "not alive." }
   </button>
 );
