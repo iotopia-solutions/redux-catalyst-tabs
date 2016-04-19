7789eb1bf791cffb3276cddb659ac200e368b274
Helen Thi <ht5658@gmail.com>
2016-04-19 14:55:33 -0400

    load text for each tab component instead of debug code to indicate whether or not tab is selected

--- src/components/tab.js
+++ src/components/tab.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default ({ children, active, tabID, makeActive }) => (
   <button className="tab" onClick={() => makeActive(tabID) }>
-    { active ? "ALIVE!!" : "not alive." }
+    { children }
   </button>
 );
